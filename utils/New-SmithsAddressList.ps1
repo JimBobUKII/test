ef64a58ba0241818e97a8d912f16e62b6cf27b82
JCHQDGEE <JCHQDGEE@5159873f-96f0-794c-8d88-a1e92769c49d>
2017-08-15 16:49:51 +0000

    New-SmithsAddressList.ps1 utility script
    git-svn-id: http://subversion.smiths.com:8000/svn/bis/operations/office365/trunk@26276 5159873f-96f0-794c-8d88-a1e92769c49d

---
+++ utils/New-SmithsAddressList.ps1
@@ -0,0 +1,52 @@
+﻿[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$true)]
+  [ValidateNotNullOrEmpty()]
+  $SiteCode, 
+  [Parameter(Mandatory=$true)]
+  [ValidateNotNullOrEmpty()]
+  $City,
+  [Parameter(Mandatory=$true)]
+  [ValidateNotNullOrEmpty()]
+  $CountryCode
+)
+
+Function main {
+
+  If($SiteCode -notmatch "^[BCDJMS][A-Z0-9]{3}$"){
+    throw "Invalid site code '$SiteCode'."
+  }
+
+  If($CountryCode -notmatch "^[A-Z]{2}$"){
+    throw "Invalid country code '$CountryCode'"
+  }
+
+  $sessions = [System.Management.Automation.Runspaces.PSSession[]](Get-PSSession  | Where { $_.ComputerName -eq "outlook.office365.com" -and $_.State -eq "Opened" })
+
+  If($sessions.Count -eq 0){
+    throw "Please connect to Exchange Online remote PowerShell and try again."
+  }
+
+  $al = [Object[]](Get-AddressList | Where { $_.DisplayName -match "^$SiteCode" })
+
+  If($al.Count -ne 0){
+    throw "An address list for site '$siteCode' already exists."
+  }
+  
+  Write-Host "Creating address list '$SiteCode - $City, $CountryCode'"
+
+  New-AddressList -Name "$SiteCode - $City, $CountryCode" -RecipientFilter "Office -eq '$SiteCode'"
+
+  $mailboxes = [Object[]](Get-Mailbox -Filter "Office -eq '$SiteCode'")
+
+  $i = 1
+
+  Foreach($mbx in $mailboxes){
+    Write-Progress -Activity "Updating address list" -PercentComplete (100*$i / $mailboxes.Count)
+    $mbx | Set-Mailbox -SimpleDisplayName $mbx.SimpleDisplayName
+    $i++
+  }
+
+}
+
+main
